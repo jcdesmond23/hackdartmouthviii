185badb8fa7db48dc8c0365737a0f10f1af500ac
jcdesmond23 <90418784+jcdesmond23@users.noreply.github.com>
2023-03-16 00:19:11 -0400

    finished draft of the first section (home)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import HomeSection from "./homeSection";
+
 export default function Home() {
   return (
     <>
+      <div className="scroll-smooth">
+        <HomeSection/>
+      </div>
     </>
   )
 }
