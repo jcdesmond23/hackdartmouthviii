3b52034b849c009b6a81c97c59c1ac9e9e3aa2b6
jcdesmond23 <90418784+jcdesmond23@users.noreply.github.com>
2023-03-20 12:42:51 -0400

    finished footer section

---
+++ src/pages/footerSection.tsx
@@ -0,0 +1,21 @@
+export default function FooterSection() {
+    return (
+      <>
+        <div id="about" className="bg-cover bg-[url('/footer.png')] h-[125px]">
+            <div className="flex flex-col h-full items-center justify-center">
+                <span className="text-[#D9D9D9] font-normal text-[23px] ">
+                    {"Are you interested in sponsoring HackDartmouth?"}
+                </span>
+                <div>
+                    <span className="text-[#D9D9D9] font-semibold text-[23px] ">
+                        {"Contact us at "}
+                    </span>
+                    <span className="text-[#D9D9D9] font-semibold underline underline-offset-4 text-[23px] ">
+                        {"team@hackdartmouth.org"}
+                    </span>
+                </div>
+            </div>
+        </div>
+      </>
+    )
+  }
--- src/pages/registrationSection.tsx
+++ src/pages/registrationSection.tsx
@@ -3,12 +3,12 @@ export default function RegistrationSection() {
       <>
         <div id="about" className="bg-cover bg-[url('/Landing2.png')] h-[1165px]">
             <div className="flex flex-col h-full items-center justify-center">
-                <h2 className="text-white text-[40px] font-black tracking-wider">WELCOME TO</h2>
-                <h1 className="text-[50px] font-black tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-[#D6AB36] to-[#D138BE]">HACKDARTMOUTH VIII</h1>
-                <h3 className="mt-1.5 text-white text-2xl font-light tracking-[.333em]">Hack to Infinity & Beyond</h3>
-                <h3 className="mt-4 text-white tracking-wider font-bold text-2xl">24 Hours  |  $8,000 in Prizes</h3>
+                <h2 className="text-white text-[40px] font-black tracking-wider">{"WELCOME TO"}</h2>
+                <h1 className="text-[50px] font-black tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-[#D6AB36] to-[#D138BE]">{"HACKDARTMOUTH VIII"}</h1>
+                <h3 className="mt-1.5 text-white text-2xl font-light tracking-[.333em]">{"Hack to Infinity & Beyond"}</h3>
+                <h3 className="mt-4 text-white tracking-wider font-bold text-2xl">{"24 Hours  |  $8,000 in Prizes"}</h3>
                 <button className="mt-12 bg-[#D9D9D9] border-2 border-[#D9D9D9] text-[#CC5264] text-[28px] font-bold tracking-widest rounded-xl py-[18px] px-16 hover:text-[#EEE9D7] hover:border-[#EEE9D7] hover:bg-gradient-to-r from-[#D6AB36] to-[#D138BE]">
-                        REGISTER
+                        {"REGISTER"}
                 </button>
             </div>
         </div>
